454f8f04a25764b5a51868b46b12dcf3e3bafbe1
MY <715711877@qq.com>
2020-02-07 11:17:43 +0800

    完成城市搜索功能

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <city-header/>
-        <city-search/>
+        <city-search :cities="cities"/>
         <city-list :cities="cities" :hot="hotCities" :letter="letter"/>
         <city-alphabet :cities="cities" @change="handleChange"/>
     </div>
--- src/views/city/components/Search.vue
+++ src/views/city/components/Search.vue
@@ -1,12 +1,63 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+    import BScroll from 'better-scroll'
     export default {
-        name: 'CitySearch'
+        name: 'CitySearch',
+        props: {
+            cities: Object
+        },
+        data() {
+            return {
+                keyword: '',
+                list: [],
+                timer: null
+            }
+        },
+        computed: {
+          hasNoData() {
+              return this.list.length === 0
+          }
+        },
+        mounted() {
+            this.scroll = new BScroll(this.$refs.search)
+        },
+        watch: {
+            keyword() {
+                if (this.timer) {
+                    clearTimeout(this.timer);
+                    this.timer = null
+                }
+                if (!this.keyword){
+                    this.list = []
+                    return
+                }
+                this.timer = setTimeout(() => {
+                    const result = []
+                    for (let i in this.cities) {
+                        this.cities[i].forEach(v => {
+                            if (v.spell.indexOf(this.keyword) > -1 || v.name.indexOf(this.keyword) > -1) {
+                                result.push(v)
+                            }
+                        })
+                    }
+                    this.list = result
+
+                }, 100)
+            }
+        }
     }
 </script>
 
@@ -25,5 +76,20 @@
             width: 100%
             text-align center
             border-radius .06rem
+            color #666
+
+    .search-content
+        overflow hidden
+        position: absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        z-index 999
+        background-color: #eee
+
+        .search-item
+            line-height: .62rem
+            padding-left: .2rem
             color #666
 </style>
