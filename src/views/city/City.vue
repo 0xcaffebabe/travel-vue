655c3e72e34c55a16c5a21a160331bd74d72a807
MY <715711877@qq.com>
2020-02-07 09:16:11 +0800

    city增加搜索栏

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <city-header/>
+        <city-search/>
         this is city page
     </div>
 </template>
 
 <script>
     import CityHeader from './components/Header'
+    import CitySearch from './components/Search'
     export default {
         name: 'City',
         components: {
-            CityHeader
+            CityHeader,
+            CitySearch
         }
     }
 </script>
---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CitySearch'
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "~@/assets/styles/variables.styl"
+    .search
+        height: .72rem
+        padding 0 .1rem
+        background-color: $bgc
+
+        .search-input
+            box-sizing border-box
+            padding 0 .1rem
+            height: .62rem
+            line-height: .62rem
+            width: 100%
+            text-align center
+            border-radius .06rem
+            color #666
+</style>
