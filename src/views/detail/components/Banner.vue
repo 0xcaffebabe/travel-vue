adf01bbd7211683aaed7fe1d7dac1892d7fa494e
MY <715711877@qq.com>
2020-02-07 14:33:58 +0800

    完成画廊组件开发

--- src/views/detail/components/Banner.vue
+++ src/views/detail/components/Banner.vue
@@ -1,19 +1,42 @@
 <template>
-    <div class="banner">
-        <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1801/5d/5d24bb249f8ca889a3.water.jpg_600x330_378727c0.jpg" alt="">
-        <div class="banner-info">
-            <div class="banner-title">澳门水舞间演出</div>
-            <div class="banner-number">
-                <span class="iconfont">&#xe662;</span>
-                45
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img"
+                 src="http://img1.qunarzz.com/sight/p0/1801/5d/5d24bb249f8ca889a3.water.jpg_600x330_378727c0.jpg"
+                 alt="">
+            <div class="banner-info">
+                <div class="banner-title">澳门水舞间演出</div>
+                <div class="banner-number">
+                    <span class="iconfont">&#xe662;</span>
+                    45
+                </div>
             </div>
         </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"/>
     </div>
 </template>
 
 <script>
+    import CommonGallary from '@/common/gallary/Gallary'
+
     export default {
-        name: 'Banner'
+        name: 'Banner',
+        data() {
+          return {
+              imgs: ["http://img1.qunarzz.com/sight/p0/1905/f7/f78db6b93d44170aa3.img.jpg_r_800x800_e4ae696b.jpg"
+              ,"http://img1.qunarzz.com/sight/p0/1905/f7/f78db6b93d44170aa3.img.jpg_r_800x800_e4ae696b.jpg"],
+              showGallary:false
+          }
+        },
+        methods: {
+            handleBannerClick() {
+                this.showGallary = true
+            },
+            handleGallaryClose() {
+                this.showGallary = false
+            }
+        },
+        components: {CommonGallary}
     }
 </script>
 
@@ -27,6 +50,7 @@
 
         .banner-img
             width: 100%
+
         .banner-info
             display flex
             position: absolute;
@@ -34,17 +58,19 @@
             right 0
             bottom 0
             line-height .6rem
-            background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8))
+            background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+
             .banner-title
                 flex 1
                 text-align left
                 font-size .32rem
                 padding .2rem
+
             .banner-number
                 padding 0 .4rem
                 line-height: .32rem
                 height .32rem
                 margin-top .2rem
                 border-radius .2rem
-                background rgba(0,0,0,.4)
+                background rgba(0, 0, 0, .4)
 </style>
